a9fc75cebd228d4b1f74f7e7a027a88e61fb8aaa
calgilbert <calvin.gilbert@dcmail.ca>
2024-04-03 21:25:01 -0400

    Comment headers

--- lab_4/part_2/main.js
+++ lab_4/part_2/main.js
@@ -1,3 +1,9 @@
+/*
+Name: Calvin Gilbert
+File: index.html
+Date: 29 March 2024
+Assignment 4: Part 1: Silly Story Generator
+*/
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
