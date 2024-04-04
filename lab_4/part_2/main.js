e3310337aead237384fa217d1e5d7202ab722623
calgilbert <calvin.gilbert@dcmail.ca>
2024-04-04 01:55:41 -0400

    Update main.js

--- lab_4/part_2/main.js
+++ lab_4/part_2/main.js
@@ -13,17 +13,20 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-const alttext = {
-'pic1.jpg' : 'Eye', 'pic2.jpg' : 'Earth', 'pic3.jpg' : 'Flowers', 'pic4.jpg' : 'Hieroglyphs', 'pic5.jpg' : 'Moth'
-}
 
 /* Declaring the alternative text for each image file */
 
-/* Looping through images */
+const alttext = {
+    'pic1.jpg' : 'Eye', 'pic2.jpg' : 'Earth', 'pic3.jpg' : 'Flowers', 'pic4.jpg' : 'Hieroglyphs', 'pic5.jpg' : 'Moth'
+    }
 
+/* Looping through images */
+for (const image of images) {
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
+
+}
 
 /* Wiring up the Darken/Lighten button */
