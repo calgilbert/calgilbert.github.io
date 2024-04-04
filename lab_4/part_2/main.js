7e1bdd28ceaf04e38487a7c423641286fa0296c3
calgilbert <calvin.gilbert@dcmail.ca>
2024-04-04 11:55:31 -0400

    Getting the images to appear

--- lab_4/part_2/main.js
+++ lab_4/part_2/main.js
@@ -1,8 +1,8 @@
 /*
 Name: Calvin Gilbert
 File: index.html
-Date: 29 March 2024
-Assignment 4: Part 1: Silly Story Generator
+Date: 03 April 2024
+Assignment 4: Part 2: Image Gallery
 */
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
@@ -17,15 +17,15 @@ const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
 
 const alttext = {
-    'pic1.jpg' : 'Eye', 'pic2.jpg' : 'Earth', 'pic3.jpg' : 'Flowers', 'pic4.jpg' : 'Hieroglyphs', 'pic5.jpg' : 'Moth'
+    'pic1.jpg' : 'Closeup of a human eye', 'pic2.jpg' : 'Earth', 'pic3.jpg' : 'Flowers', 'pic4.jpg' : 'Hieroglyphs', 'pic5.jpg' : 'Moth'
     }
 
 /* Looping through images */
 for (const image of images) {
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alttext[image]);
+    thumbBar.appendChild(newImage);
 
 }
 
