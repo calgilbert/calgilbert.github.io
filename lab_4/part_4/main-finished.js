1645313e98248156156cdf6ec672e72ae602d89b
calgilbert <calvin.gilbert@dcmail.ca>
2024-04-05 01:46:34 -0400

    Restarting with a new file
    Something went very wrong I feel.

--- lab_4/part_4/main-finished.js
+++ lab_4/part_4/main-finished.js
@@ -1,9 +1,3 @@
-/*
-Name: Calvin Gilbert
-File: index.html
-Date: 04 April 2024
-Assignment 4: Part 4: Editing Ball Bounce Demo to Add More Features
-*/
 // set up canvas
 
 const canvas = document.querySelector("canvas");
@@ -64,92 +58,20 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball) && ball.exists) {
+      if (!(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-  
+
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
       }
     }
-  }  
-}
-
-class EvilCircle extends Shape {
-  constructor(x, y) {
-    super(x, y, 20, 20);
-    this.size = 10;
-    this.color = "white"
-  
-    window.addEventListener("keydown", (e) => {
-      switch (e.key) {
-        case "a":
-          this.x -= this.velX;
-          break;
-        case "d":
-          this.x += this.velX;
-          break;
-        case "w":
-          this.y -= this.velY;
-          break;
-        case "s":
-          this.y += this.velY;
-          break;
-      }
-    });
-    
   }
-
-draw() {
-  ctx.beginPath();
-    ctx.strokeStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.lineWidth = 3;
-}
-  
-checkBounds() {
-  if (this.x + this.size >= width) {
-    this.x -= this.size;
-  }
-
-  if (this.x - this.size <= 0) {
-    this.x += this.size;
-  }
-
-  if (this.y + this.size >= height) {
-    this.y -= this.size;
-  }
-
-  if (this.y - this.size <= 0) {
-    this.y += this.size;
-  }
-}
-
-collisionDetect() {
-  for (const ball of balls) {
-    if (ball.exists) {
-      const dx = this.x - ball.x;
-      const dy = this.y - ball.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + ball.size) {
-        ball.exists = false;
-
-
-      }}}    
-}
 }
 
 const balls = [];
-
-const playerBall = EvilCircle(random(width), random(height));
-
-playerBall.draw();
-playerBall.checkBounds();
-playerBall.collisionDetect();
 
 while (balls.length < 25) {
   const size = random(10, 20);
@@ -172,14 +94,10 @@ function loop() {
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    if (ball.exists) {
     ball.draw();
     ball.update();
     ball.collisionDetect();
   }
-}
-
-
 
   requestAnimationFrame(loop);
 }
