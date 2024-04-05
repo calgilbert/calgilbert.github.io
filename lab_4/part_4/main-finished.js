90823ff2a0248162c719953c75a22153c244d853
calgilbert <calvin.gilbert@dcmail.ca>
2024-04-05 02:01:23 -0400

    Yeah I just can't get the html this to process.

--- lab_4/part_4/main-finished.js
+++ lab_4/part_4/main-finished.js
@@ -71,7 +71,75 @@ class Ball {
   }
 }
 
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.size = 10;
+    this.color = "white"
+  
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+    
+  }
+
+draw() {
+  ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.lineWidth = 3;
+}
+  
+checkBounds() {
+  if (this.x + this.size >= width) {
+    this.x -= this.size;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.x += this.size;
+  }
+
+  if (this.y + this.size >= height) {
+    this.y -= this.size;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.y += this.size;
+  }
+}
+
+collisionDetect() {
+  for (const ball of balls) {
+    if (ball.exists) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.exists = false;
+        }
+      }
+    }
+  }
+}
+
 const balls = [];
+
+const playerBall = EvilCircle();
 
 while (balls.length < 25) {
   const size = random(10, 20);
@@ -99,6 +167,9 @@ function loop() {
     ball.collisionDetect();
   }
 
+  playerBall.draw();
+  playerBall.checkBounds();
+  playerBall.collisionDetect();
   requestAnimationFrame(loop);
 }
 
